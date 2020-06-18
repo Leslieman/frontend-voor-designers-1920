f9649521a17072e8e8df346460d78967d81845b0
Leslieman <48986838+Leslieman@users.noreply.github.com>
2020-06-18 11:43:02 +0200

    -

--- opdracht3/js/script.js
+++ opdracht3/js/script.js
@@ -1,8 +1,15 @@
-var pageCounter = 1;
+
 var header = document.querySelector('h1');
 var btn = document.querySelector('#btn');
 var info = document.querySelector('.animal-info');
 var request = new XMLHttpRequest();
+
+var counter = 1; //teller om door de objecten te rouleren
+
+
+var pageCounter = 1; //teller om door de 3 json pagina's te rouleren 
+var pageCounterTwo = 1; //teller om de eerste teller te helpen
+
 
 btn.addEventListener("click", function(){
     request.open('GET', 'https://learnwebcode.github.io/json-example/animals-'+ pageCounter +'.json');
@@ -13,17 +20,81 @@ const data = request.response;
 renderHTML(data);
 };
 request.send();
-pageCounter++;
-if (pageCounter > 3){
-    pageCounter = 1;
-}
+
+
 }); 
+
+
+
+
+//luistert naar keydown event van de spacebar
+window.addEventListener('keydown', checkKeyPress);
+
+function checkKeyPress(key){
+    if (key.keyCode == 32){
+        request.open('GET', 'https://learnwebcode.github.io/json-example/animals-'+ pageCounter +'.json');
+        request.responseType = 'json';
+    
+    request.onload = function(){
+    const data = request.response;  
+    renderHTML(data);
+    };
+    request.send();
+    
+    }
+};
 
 function renderHTML(data){
     var htmlString ="";
-    for (i = 0; i < data.length; i++){
-        htmlString += "<p>" + data[i].name + " is a " + data[i].species + " and likes: " + data[i].foods.likes + "</p>";
+    
+    if (counter === 1){
+        htmlString += "<p>" + "<img src='images/01/01.jpg'>" + data[0].name + " is a " + data[0].species + " and likes: " + data[0].foods.likes + "</p>";
+        counter++;
+        info.insertAdjacentHTML('beforeend', htmlString);
     }
-
-    info.insertAdjacentHTML('beforeend', htmlString);
+    else if (counter === 2){
+        htmlString += "<p>" + "<img src='images/01/03.jpg'>" + data[1].name + " is a " + data[1].species + " and likes: " + data[1].foods.likes + "</p>";
+        counter++;
+        info.insertAdjacentHTML('beforeend', htmlString);
+    }
+    else if (counter === 3){
+        htmlString += "<p>" + "<img src='images/01/02.jpg'>" + data[2].name + " is a " + data[2].species + " and likes: " + data[2].foods.likes + "</p>";        
+        pageCounter++;
+        counter++;
+        info.insertAdjacentHTML('beforeend', htmlString);
+    }
+    else if (counter === 4){
+        htmlString += "<p>" + "<img src='images/02/01.jpg'>" + data[0].name + " is a " + data[0].species + " and likes: " + data[0].foods.likes + "</p>";
+        counter++;
+        info.insertAdjacentHTML('beforeend', htmlString);
+    }
+    else if (counter === 5){
+        htmlString += "<p>" + "<img src='images/02/03.jpg'>" + data[1].name + " is a " + data[1].species + " and likes: " + data[1].foods.likes + "</p>";
+        counter++;
+        info.insertAdjacentHTML('beforeend', htmlString);
+    }
+    else if (counter === 6){
+        htmlString += "<p>" + "<img src='images/02/02.jpg'>" + data[2].name + " is a " + data[2].species + " and likes: " + data[2].foods.likes + "</p>";
+        counter++;
+        pageCounter++;
+        info.insertAdjacentHTML('beforeend', htmlString);
+    }
+    else if (counter === 7){
+        htmlString += "<p>" + "<img src='images/03/01.jpg'>" + data[0].name + " is a " + data[0].species + " and likes: " + data[0].foods.likes + "</p>";
+        counter++;
+        info.insertAdjacentHTML('beforeend', htmlString);
+    }
+    else if (counter === 8){
+        htmlString += "<p>" + "<img src='images/03/03.jpg'>" + data[1].name + " is a " + data[1].species + " and likes: " + data[1].foods.likes + "</p>";
+        counter++;
+        info.insertAdjacentHTML('beforeend', htmlString);
+    }
+    else if (counter === 9){
+        htmlString += "<p>" + "<img src='images/03/02.jpg'>" + data[2].name + " is a " + data[2].species + " and likes: " + data[2].foods.likes + "</p>";
+        counter = 1;
+        pageCounter = 1;
+        info.insertAdjacentHTML('beforeend', htmlString);
+    }
+        
 };
+
---
+++ opdracht3/readme.md
@@ -0,0 +1,10 @@
+## Description
+
+Ik heb voor de eindopdracht heb ik een json gebruikt met dieren. In de objecten staat de naam en favoriete eten van het desbetreffende dier. Met javascript heb ik deze objecten uit de json ingeladen in de HTML als een string en verbonden aan afbeeldingen. Javascript luistert naar twee vormen van input; button klik en keydown event (spacebar)
+
+
+## Link
+
+
+Demo: https://leslieman.github.io/frontend-voor-designers-1920/opdracht3/
+Source code: https://github.com/Leslieman/frontend-voor-designers-1920/tree/master/opdracht3
